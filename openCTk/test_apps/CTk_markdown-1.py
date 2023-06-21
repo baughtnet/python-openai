c4e2638c5c557ef521ac08d08f8e8a7d882cc79a
baugh-kss <david.n.baugh.com>
2023-06-20 20:17:23 -0700

    code fix works, layout for code gen started, progress made on markdown, main hang up is rendering dynamic html

--- openCTk/test_apps/CTk_markdown-1.py
+++ openCTk/test_apps/CTk_markdown-1.py
@@ -7,28 +7,28 @@ from customtkinter.windows.widgets import appearance_mode
 import openai
 import os
 
+global gpt_html
+
 # import openAI API key
-# openai.api_key = os.environ.get('OPENAI_API')
-openai.api_key = "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"
+openai.api_key = os.environ.get('OPENAI_API')
 # messages = [
     # {"role": "user", "content": "You are a polite and helpful assistant"}
 #]
 
 messages = [
         {'role': "user",
-         "content": "You are a polite and helpful assistant.  Please summarize ideas with bullet points where appropriate, like after a paragraph of text explaining a concept for example.  Also make use of comparisons and/or analogies where appropriate.  Your responses shouldn't come off as patronizing or condescending."}
+         "content": "You are a polite and helpful assistant.  Please summarize ideas with bullet points where appropriate, like after a paragraph of text explaining a concept for example.  Also make use of comparisons and/or analogies where appropriate.  I remember these are suggestions, do not over use the suggested ideas in the last few sentences, only where it would make sense to do so.  Finally, your responses shouldn't come off as patronizing or condescending."}
 ]
 
 # update_win function for sending prompt and clearing the prompt box
 def update_win(event):
     global content
+    global gpt_html
+    gpt_html = "<h2>welcome to chatGPT baughtnet edition</h2><br>"
     content = txt_prompt.get(1.0, "end-1c")
+    gpt_html = gpt_html + content
     # txt_gpt.insert("end", "You say:  " + content + "\n" + "---------------------------------------------------------------" + "\n")
     # txt_prompt.delete(1.0, 'end')
-    chat()
-
-# chat function for adding AI response to txt_gpt text box
-def chat():
     messages.append({"role": "user", "content": content})
     completion = openai.ChatCompletion.create(
         model = "gpt-3.5-turbo-16k",
@@ -41,11 +41,13 @@ def chat():
     html_response = html_response.replace('<pre><code>', '<pre><code class="code-block">')
     html_response = html_response.replace('<pre><code class="code-block">', '<pre class="code-block-wrapper"><code class="code-block">')
     html_response = html_response.replace('</code></pre>', '</code></pre><button class="copy-button">Copy</button>')
-    html_gpt.html = html_response
-
+    gpt_html = gpt_html + html_response
+    html_gpt = HTMLText(main_frame, html=gpt_html)
+    html_gpt.grid(columnspan=2, row=0, column=1, padx=5, pady=5, sticky='nsew')
     # txt_gpt.insert("end", html_response)
     # txt_gpt.see('end')
     print(chat_response)
+    print(gpt_html)
 
 # Set up app appearance
 customtkinter.set_appearance_mode("Dark")
@@ -85,8 +87,8 @@ main_frame.rowconfigure(1)
 # text boxes for input and response
 # txt_gpt = customtkinter.CTkTextbox(master=main_frame)
 # txt_gpt.grid(columnspan=2, row=0, column=1, padx=5, pady=5, sticky='nsew')
-html_gpt = HTMLText(main_frame, html="")
-html_gpt.grid(columnspan=2, row=0, column=1, padx=5, pady=5, sticky='nsew')
+# html_gpt = HTMLText(main_frame, html=gpt_html)
+# html_gpt.grid(columnspan=2, row=0, column=1, padx=5, pady=5, sticky='nsew')
 txt_prompt = customtkinter.CTkTextbox(master=main_frame)
 txt_prompt.grid(row=1, column=1, padx=5, pady=5, sticky='nsew')
 
