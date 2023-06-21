808174b4b713c828cd67c435be4e072e1dd932f6
dbaugh <114878979+baughtnet@users.noreply.github.com>
2023-06-20 23:11:23 -0700

    small tweaks, couple more needed, fairly functional

--- openCTk/openCTk-v3.py
+++ openCTk/openCTk-v3.py
@@ -67,8 +67,9 @@ def code_help(event):
 
 def code_create(event):
     global codeCreate_prompt
-    code = txt_code_gen.get(1.0, "end-1c")
-    error = txt_prompt_gen.get(1.0, "end-1c")
+    code = txt_prompt_gen.get(1.0, "end-1c")
+    txt_code_gen.insert("end", "You say: " + code + "\n" + "---------------------------------------------------------------" + "\n")
+    txt_prompt_gen.delete(1.0, 'end')
     codeCreate_prompt = code
 
     create_messages.append({"role": "user", "content": codeCreate_prompt})
@@ -103,9 +104,6 @@ tab_view.add("Normal")
 tab_view.add("Code Fix")
 tab_view.add("Code Gen")
 
-# btn_frame = customtkinter.CTkFrame(app)
-# btn_frame.grid(padx=5, pady=5, sticky='se')
-
 # configure main frame for normal tab
 main_frame = customtkinter.CTkFrame(tab_view.tab("Normal"))
 main_frame.pack(fill='both', expand=True, padx=5, pady=5)
@@ -118,12 +116,12 @@ code_frame.pack(fill='both', expand=True, padx=5, pady=5)
 code_gen_frame = customtkinter.CTkFrame(tab_view.tab("Code Gen"))
 code_gen_frame.pack(fill='both', expand=True, padx=5, pady=5)
 
-# configure rows and columns of the code_frame
-code_gen_frame.columnconfigure(0)
-code_gen_frame.columnconfigure(1, weight=8)
-code_gen_frame.columnconfigure(2)
-code_gen_frame.rowconfigure(0, weight=4)
-code_gen_frame.rowconfigure(1)
+# configure rows and columns of the main_frame
+main_frame.columnconfigure(0)
+main_frame.columnconfigure(1, weight=8)
+main_frame.columnconfigure(2)
+main_frame.rowconfigure(0, weight=8)
+main_frame.rowconfigure(1)
 
 # configure rows and columns of the code_frame
 code_frame.columnconfigure(0)
@@ -133,20 +131,26 @@ code_frame.rowconfigure(0, weight=4)
 code_frame.rowconfigure(1)
 code_frame.rowconfigure(2)
 
-# configure elements in code_gen_frame
-txt_code_gen = customtkinter.CTkTextbox(master=code_gen_frame, wrap='word')
-txt_code_gen.grid(columnspan=2, row=0, column=1, padx=5, pady=5, sticky='nsew')
-txt_prompt_gen = customtkinter.CTkTextbox(master=code_gen_frame, wrap='word')
-txt_prompt_gen.grid(columnspan=2, row=1, column=1, padx=5, pady=5, sticky='nsew')
+# configure rows and columns of the code_gen_frame
+code_gen_frame.columnconfigure(0)
+code_gen_frame.columnconfigure(1, weight=8)
+code_gen_frame.columnconfigure(2)
+code_gen_frame.rowconfigure(0, weight=4)
+code_gen_frame.rowconfigure(1)
 
-lbl_code_gen_gpt = customtkinter.CTkLabel(master=code_gen_frame, text="Chat Says:")
-lbl_code_gen_gpt.grid(row=0, column=0, padx=5, pady=5, sticky='ne')
+# configure elements in main_frame 
+txt_gpt = customtkinter.CTkTextbox(master=main_frame, wrap='word')
+txt_gpt.grid(columnspan=2, row=0, column=1, padx=5, pady=5, sticky='nsew')
+txt_prompt = customtkinter.CTkTextbox(master=main_frame, wrap='word')
+txt_prompt.grid(row=1, column=1, padx=5, pady=5, sticky='nsew')
 
-lbl_code_gen_input = customtkinter.CTkLabel(master=code_gen_frame, text="Current Code")
-lbl_code_gen_input.grid(row=1, column=0, padx=5, pady=5, sticky='nsew')
+lbl_gpt = customtkinter.CTkLabel(master=main_frame, text="Chat says:")
+lbl_gpt.grid(row=0, column=0, padx=5, pady=5, sticky='ne')
+lbl_prompt = customtkinter.CTkLabel(master=main_frame, text="Prompt:")
+lbl_prompt.grid(row=1, column=0, padx=5, pady=5, sticky='ne')
 
-btn_code_gen = customtkinter.CTkButton(master=code_gen_frame, width=20, text="Send", command=lambda: code_create(""))
-btn_code_gen.grid(row=1, column=2, padx=5, pady=15, sticky='ns')
+btn_chat = customtkinter.CTkButton(master=main_frame, width=20, text="Send", command=lambda: chat(""))
+btn_chat.grid(row=1, column=2, padx=5, pady=15, sticky='ns')
 
 # configure elements in code_frame
 txt_code_gpt = customtkinter.CTkTextbox(master=code_frame, wrap='word')
@@ -166,34 +170,24 @@ lbl_code_error.grid(row=2, column=0, padx=5, pady=5, sticky='nsew')
 btn_code = customtkinter.CTkButton(master=code_frame, text="Send", width=20, command=lambda: code_help(""))
 btn_code.grid(row=2, column=2, pady=15, padx=5, sticky='ns')
 
-# configure rows and columns of the main_frame
-main_frame.columnconfigure(0)
-main_frame.columnconfigure(1, weight=8)
-main_frame.columnconfigure(2)
-main_frame.rowconfigure(0, weight=8)
-main_frame.rowconfigure(1)
+# configure elements in code_gen_frame
+txt_code_gen = customtkinter.CTkTextbox(master=code_gen_frame, wrap='word')
+txt_code_gen.grid(columnspan=2, row=0, column=1, padx=5, pady=5, sticky='nsew')
+txt_prompt_gen = customtkinter.CTkTextbox(master=code_gen_frame, wrap='word')
+txt_prompt_gen.grid(row=1, column=1, padx=5, pady=5, sticky='nsew')
 
-# text boxes for input and response
-txt_gpt = customtkinter.CTkTextbox(master=main_frame, wrap='word')
-txt_gpt.grid(columnspan=2, row=0, column=1, padx=5, pady=5, sticky='nsew')
+lbl_code_gen_gpt = customtkinter.CTkLabel(master=code_gen_frame, text="Chat Says:")
+lbl_code_gen_gpt.grid(row=0, column=0, padx=5, pady=5, sticky='ne')
+lbl_code_gen_input = customtkinter.CTkLabel(master=code_gen_frame, text="Current Code")
+lbl_code_gen_input.grid(row=1, column=0, padx=5, pady=5, sticky='nsew')
 
-txt_prompt = customtkinter.CTkTextbox(master=main_frame, wrap='word')
-txt_prompt.grid(row=1, column=1, padx=5, pady=5, sticky='nsew')
-
-# labels for prompt and chat says
-lbl_gpt = customtkinter.CTkLabel(master=main_frame, text="Chat says:")
-lbl_gpt.grid(row=0, column=0, padx=5, pady=5, sticky='ne')
-lbl_prompt = customtkinter.CTkLabel(master=main_frame, text="Prompt:")
-lbl_prompt.grid(row=1, column=0, padx=5, pady=5, sticky='ne')
-
-# buttons
-btn_chat = customtkinter.CTkButton(master=main_frame, width=20, text="Send", command=lambda: chat(""))
-btn_chat.grid(row=1, column=2, padx=5, pady=15, sticky='ns')
+btn_code_gen = customtkinter.CTkButton(master=code_gen_frame, width=20, text="Send", command=lambda: code_create(""))
+btn_code_gen.grid(row=1, column=2, padx=5, pady=15, sticky='ns')
 
 btn_close = customtkinter.CTkButton(app, text="Exit", command=app.destroy)
 btn_close.pack(padx=5, pady=5, side='right')
 
-app.bind("<Shift-Return>", lambda event: chat(""))
+# app.bind("<Shift-Return>", lambda event: chat(""))
 
 # run the app
 app.mainloop()
