911febd56d12d90ef17999760843b14fda110f0d
baugh-kss <134890775+baugh-kss@users.noreply.github.com>
2023-12-20 10:14:38 -0800

    Revert "Revert "initial commit for new branch, mostly working... need to verify request to openai is sent and received back currently working on trying to get userInput and response to print in the webpage """
    This reverts commit 53408315b008feedea3b9d249589efd9846fc936.
    
    "committed these files to main instead of flaskGPT branch"

---
+++ flaskGPT/v1/static/js/script.js
@@ -0,0 +1,35 @@
+const chatHistory = document.getElementById('chat-history');
+const userInput = document.getElementById('user-input');
+const chatForm = document.getElementById('chat-form');
+
+chatForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  // Send user input to server using AJAX
+  fetch('/chat', {
+    method: 'POST',
+    body: JSON.stringify({ user_input: userInput.value }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    // Update chat history with server response
+    chatHistory.innerHTML += `<div class="message ${data.role}">${data.content}</div>`;
+    userInput.value = '';
+
+    // Additional logic for scrolling, highlighting, etc.
+
+  })
+  .catch(error => {
+    console.error(error);
+  });
+});
+
+// Add event listener for enter key (optional)
+userInput.addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
+    userInput.submit();
+  }
+});
